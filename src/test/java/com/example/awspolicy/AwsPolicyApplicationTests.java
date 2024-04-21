a019922e50b5148ec09740ccfd13575cdbe73ca2
peter20011 <piotr.warcholek@student.pk.edu.pl>
2024-04-21 16:50:11 +0200

    init

---
+++ src/test/java/com/example/awspolicy/AwsPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awspolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPolicyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
